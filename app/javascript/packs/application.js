aa663c1eafbf6af1708d699b84244c76b63d7090
Shafaq Nawaz <shafaqnawaz784@gmail.com>
2022-08-08 13:44:12 +0500

    Import Bootstrap Added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import "bootstrap";
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import "bootstrap"
+=======
+import('sb-admin-2');
+>>>>>>> Stashed changes
+=======
+import('sb-admin-2');
+>>>>>>> Stashed changes
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
