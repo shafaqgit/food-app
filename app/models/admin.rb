21b5a47dbc7eefab24cee0b0bc7706b6ee4a35a1
Shafaq Nawaz <shafaqnawaz784@gmail.com>
2022-08-11 15:35:02 +0500

    comment added

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
- #comment added
+ #comment added to file
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
