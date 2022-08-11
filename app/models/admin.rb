602348afe647cec5fd0b98e3c818ae01e51c56f2
Shafaq Nawaz <shafaqnawaz784@gmail.com>
2022-08-11 15:53:22 +0500

    employee table updates

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,8 +2,7 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
- #comment added to file
- #hello
+ #field comment added 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
